cdd29338b6639981008bf9d17bde73bc3e988eb2
gjoa <gjoa@pop-os.localdomain>
2019-12-29 13:38:03 +0000

    Simplified tree properties. Temperature is now the only property trees utilise to evolve.

--- Code/CropController.gd
+++ Code/CropController.gd
@@ -3,53 +3,42 @@ extends Spatial
 var crop: Resource = load("res://Scenes/Crop.tscn")
 
 var crop_timer: float = 0.0
-
 var death_time: float = 0.5
 
 #CROP PROPERTIES
-var growth_rate: float = 0
-var seed_success_rate: float = 0 #Probability that a seed grows into a crop.
-var disease_resistance: float = 0 #Probability that a crop doesn't die from disease.
-var num_seeds: int = 0 #Number of seeds a crop will drop on death.
+var growth_rate: float = 0.5
+var perfect_temp: float = 15
+
+var mutation_chance: float = 0.01
 
 func _ready() -> void:
-	var timer: Timer = Timer.new()
-	timer.connect("timeout", self, "_on_timer_timeout")
-	add_child(timer)
-	timer.start()
-	timer.set_wait_time(3)
-	death_time = rand_range(3, 5)
+	death_time = rand_range(2, 3)
 	get_parent().crop_arr.append(self)
-
-
-#Randomises this seed's traits.
-func set_random_traits() -> void:
-	growth_rate = rand_range(0.0, 1.0)
-	seed_success_rate = rand_range(0.5, 0.75)
-	num_seeds = randi() % 5 + 3
-	disease_resistance = randf()
 		
 func set_mesh() -> void:
 	var leaves: SpatialMaterial = SpatialMaterial.new()
 	var bark: SpatialMaterial = SpatialMaterial.new()
 	bark.albedo_color = Color(1, 1, 1)
-	#leaves.albedo_color = Color(clamp(growth_rate, 0, 1), clamp(seed_success_rate, 0, 1), clamp(num_seeds, 0, 1))
-	leaves.albedo_color = Color(disease_resistance, 0, 0)
+	
+	var red: Color = Color(1, 0, 0)
+	var blue: Color = Color(0, 0, 1)
+	var c: Color = blue.linear_interpolate(red, perfect_temp / 30)
+	leaves.albedo_color = c
 	$MeshInstance.set_surface_material(0, leaves)
 	$MeshInstance.set_surface_material(1, bark)
-
+	
+func set_traits(s: Spatial) -> void:
+	if (randf() < mutation_chance):
+		s.perfect_temp = perfect_temp * (1.0 + rand_range(-0.5, 0.5))
+	else:
+		s.perfect_temp = perfect_temp * (1.0 + rand_range(-0.05, 0.05))
 
 #Handles behaviour when a crop is set to die.
 func reproduce(max_angle: float, max_seeds: int) -> void:
-	for i in range(max_seeds):
-		#If the seed is successful
-		if randf() < seed_success_rate && get_parent().num_entities < get_parent().get_max_entities():
-			
-			get_parent().num_entities += 1
-
+	if can_reproduce() == true:
+		for i in range(max_seeds):
+			#If the seed is successful
 			var foo: Spatial = crop.instance()
-			get_parent().crop_arr.append(foo) #Add foo to the list of crops.
-
 			get_parent().add_child(foo)
 			#So basis is same as parent node's.
 			foo.rotation = rotation 
@@ -60,37 +49,34 @@ func reproduce(max_angle: float, max_seeds: int) -> void:
 			var look_dir: Vector3 = new_pos.normalized().cross(Vector3.ONE)
 			foo.translation += new_pos
 			foo.look_at(foo.translation + look_dir * 100, new_pos.normalized()) 
-
-			foo.growth_rate = growth_rate 
-			foo.seed_success_rate = seed_success_rate 
-			foo.num_seeds = num_seeds
-			foo.disease_resistance = disease_resistance * (1 + rand_range(-0.05, 0.5))
+			set_traits(foo)
 			foo.set_mesh()
+	kill()
 
-	get_parent().crop_arr.erase(self)
-	queue_free()
-	get_parent().num_entities -= 1
+func can_reproduce() -> bool:
+	var temp_delta: float = abs(perfect_temp - get_parent().temperature)
+	var prob: float = 1 - (temp_delta / perfect_temp)
+	if randf() > prob:
+		return false
+	return true
 
 #To be called when wanting to destroy a crop.
 func kill() -> void:
 	get_child(0).queue_free() #Delete child (timer).
-	get_parent().crop_arr.erase(self) #Remove from list of crops.
+	var i: int = get_parent().crop_arr.find(self)
+	get_parent().crop_arr.remove(i)
 	queue_free() #Queue for deletion.
-
-func disease_chance() -> void:
-	if randf() > disease_resistance:
-		kill()
 
 func grow_crop(timer: float, delta: float) -> void:
 	#If there are future stages available
 	if timer < death_time:
 		scale += Vector3.ONE * growth_rate * delta
 	else:
-		reproduce(30, num_seeds)
-
-func _on_timer_timeout() -> void:
-	disease_chance()
-		
+		if randf() < 0.05:
+			reproduce(30, 2)
+		else:
+			reproduce(30, 1)
+					
 func _physics_process(delta: float) -> void:
 	crop_timer += delta
 	grow_crop(crop_timer, delta)
--- Code/WorldController.gd
+++ Code/WorldController.gd
@@ -6,13 +6,33 @@ var num_entities = 0
 
 var crop_arr = []
 
+var t: Timer = Timer.new()
+
 func get_max_entities() -> int:
 	return max_entities
 
 func _ready() -> void:
-	pass # Replace with function body.
+	t.connect("timeout", self, "_on_timer_timeout")
+	add_child(t)
+	t.set_wait_time(1)
+	t.start()
 
+func get_average_perfect_temperature() -> float:
+	var total: float = 0
+	for i in crop_arr:
+		total += i.perfect_temp
+	if crop_arr.size() > 0:
+		return total / crop_arr.size() 
+	else:
+		return 0.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_timer_timeout() -> void:
+	print("Average ideal temperature: " + str(get_average_perfect_temperature()))
+	print("New temperature is: " + str(temperature))
+	print("Number of trees: " + str(crop_arr.size()))
+	print("----------------------")
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("temp_up"):
+		temperature += 0.05
+		print("Temperature is: " + str(temperature))
